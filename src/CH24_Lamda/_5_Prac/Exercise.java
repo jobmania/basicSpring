91f7ec139ff8a617d0ad6018297f15cc0a8b27ff
Son won cheol <xzxz7003@naver.com>
2023-02-23 11:33:11 +0900

    예제 ++

--- src/CH24_Lamda/_5_Prac/Exercise.java
+++ src/CH24_Lamda/_5_Prac/Exercise.java
@@ -23,9 +23,12 @@ public class Exercise {
          */
 
         list.removeIf(i -> i < 0 || i >10); // 0~10 값이 아닌 값들은 모두 제거
-        list.forEach( i -> System.out.println(i));
+        list.forEach(System.out::println);
         list.replaceAll(i -> i + 100); // 각 아이템들에 + 100 을 한다.
-        list.forEach( i -> System.out.println(i));
+        list.forEach(i -> System.out.println(i));
+        // list.forEach(::); [인스턴스]::[메소드명(또는 new)] / 람다식이 건네는 파라미터와 받는 쪽의 파라미터가 동일할 때
+        // println이 메소드명
+        list.forEach(System.out::println);
 
 
 
